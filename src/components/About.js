7cc34eb9fa7e870459fa195e0aae8c4fb7756bb2
Egor Marvelous <marvelchenko@gmail.com>
2024-12-24 14:15:41 +0100

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
    <>
-    <div className='w-full '>
+    <div className='w-full flex flex-wrap'>
       <div className='w-full flex justify-center text-center p-10'>
       <div className='w-[25rem] p-3'>
       <h3 className='text-2xl'>Plus de simplicité, de flexibilité, de design et d'échanges</h3>
